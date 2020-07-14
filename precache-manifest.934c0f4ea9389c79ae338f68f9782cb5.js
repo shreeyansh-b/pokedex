48120ba3c73ba3da87eafb36caa3acb46387c70b
Shreeyansh <shebucr7@gmail.com>
2020-07-14 17:44:30 +0530

    Updates

---
+++ precache-manifest.934c0f4ea9389c79ae338f68f9782cb5.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9f103b5065d47dfece862bbbe82bb2",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "003beb174d0bb8eb6190",
+    "url": "/pokedex/static/css/main.20ce6960.chunk.css"
+  },
+  {
+    "revision": "4d02ea9e4502c7410cc4",
+    "url": "/pokedex/static/js/2.839916d9.chunk.js"
+  },
+  {
+    "revision": "2715dfca609a4cf544db7a6013222897",
+    "url": "/pokedex/static/js/2.839916d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "003beb174d0bb8eb6190",
+    "url": "/pokedex/static/js/main.fda61035.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  },
+  {
+    "revision": "22260ad4e77375b4e7f358eaa905eef1",
+    "url": "/pokedex/static/media/attack.22260ad4.svg"
+  },
+  {
+    "revision": "dada0b6152c985a92bbb89f36d9bb88d",
+    "url": "/pokedex/static/media/bug.dada0b61.svg"
+  },
+  {
+    "revision": "8ee397f7885ee2237384151df614bdf7",
+    "url": "/pokedex/static/media/dark.8ee397f7.svg"
+  },
+  {
+    "revision": "91140dd74baf6b0cd8c1e6dc826d190e",
+    "url": "/pokedex/static/media/defense.91140dd7.svg"
+  },
+  {
+    "revision": "0490a514d8e003bfa514edb106b85233",
+    "url": "/pokedex/static/media/dragon.0490a514.svg"
+  },
+  {
+    "revision": "33f62224604b8d1cce749cb3fbe97222",
+    "url": "/pokedex/static/media/electric.33f62224.svg"
+  },
+  {
+    "revision": "768bc87fcb950e2e3ae6a8613290310a",
+    "url": "/pokedex/static/media/fairy.768bc87f.svg"
+  },
+  {
+    "revision": "af4b1e2a2d684d4541ca2bbd99705583",
+    "url": "/pokedex/static/media/fighting.af4b1e2a.svg"
+  },
+  {
+    "revision": "dd044703ce808b9e7bff494d2936cb3c",
+    "url": "/pokedex/static/media/fire.dd044703.svg"
+  },
+  {
+    "revision": "568c0529817f399a5ad50069e9c87720",
+    "url": "/pokedex/static/media/flying.568c0529.svg"
+  },
+  {
+    "revision": "9297e75e141c7ffd2389395151e963d2",
+    "url": "/pokedex/static/media/ghost.9297e75e.svg"
+  },
+  {
+    "revision": "5fcf7ce8a4276ca02381f7ff208f6305",
+    "url": "/pokedex/static/media/grass.5fcf7ce8.svg"
+  },
+  {
+    "revision": "3fae01972ffba3840cc83cea8a53f6e4",
+    "url": "/pokedex/static/media/ground.3fae0197.svg"
+  },
+  {
+    "revision": "679df417aeafcda4c85e6cd34ee9445b",
+    "url": "/pokedex/static/media/hp.679df417.svg"
+  },
+  {
+    "revision": "792f0fda106d4666380a9f503e8d8ce1",
+    "url": "/pokedex/static/media/ice.792f0fda.svg"
+  },
+  {
+    "revision": "3c5efcde703bf15776782bf093aad192",
+    "url": "/pokedex/static/media/landing.3c5efcde.gif"
+  },
+  {
+    "revision": "bda662acbfd7d1580620314b5e0f79ad",
+    "url": "/pokedex/static/media/landing2.bda662ac.gif"
+  },
+  {
+    "revision": "f856103173adde95e5a0bde2a7c18242",
+    "url": "/pokedex/static/media/landing3.f8561031.gif"
+  },
+  {
+    "revision": "f615900e14660d5a06373829e50cff14",
+    "url": "/pokedex/static/media/landing4.f615900e.gif"
+  },
+  {
+    "revision": "cde9e2b5d58b087e5bdbc6305e1acbb7",
+    "url": "/pokedex/static/media/landing5.cde9e2b5.gif"
+  },
+  {
+    "revision": "f096658a18d8da46980d8a13309c083e",
+    "url": "/pokedex/static/media/landing6.f096658a.gif"
+  },
+  {
+    "revision": "57c8e5afad80f523857754efdbecdc8f",
+    "url": "/pokedex/static/media/landing7.57c8e5af.gif"
+  },
+  {
+    "revision": "0e6ec2f0c28eab6f95422a2763025d18",
+    "url": "/pokedex/static/media/loading.0e6ec2f0.gif"
+  },
+  {
+    "revision": "90d2bcd49007af5f8ac2ecd434eee1bd",
+    "url": "/pokedex/static/media/loading2.90d2bcd4.gif"
+  },
+  {
+    "revision": "65aacb019a9525658008a611651822c5",
+    "url": "/pokedex/static/media/loading3.65aacb01.gif"
+  },
+  {
+    "revision": "4ec0a4c01f37ab292bed7f0bee2389e6",
+    "url": "/pokedex/static/media/loading4.4ec0a4c0.gif"
+  },
+  {
+    "revision": "da06026379392aa48f0559b313b1fc0f",
+    "url": "/pokedex/static/media/logo.da060263.png"
+  },
+  {
+    "revision": "0c17a4c577bbb1c5a49b6b65b087db91",
+    "url": "/pokedex/static/media/normal.0c17a4c5.svg"
+  },
+  {
+    "revision": "85db796217b4e20f5c3b50fdb4ddb99c",
+    "url": "/pokedex/static/media/poison.85db7962.svg"
+  },
+  {
+    "revision": "18ce2303dde4bdb42b2184aaaf6d1739",
+    "url": "/pokedex/static/media/psychic.18ce2303.svg"
+  },
+  {
+    "revision": "c79149f6d4013427162f28052de45119",
+    "url": "/pokedex/static/media/rock.c79149f6.svg"
+  },
+  {
+    "revision": "31edfa91ee1d554236d26ede199b6a23",
+    "url": "/pokedex/static/media/special-attack.31edfa91.svg"
+  },
+  {
+    "revision": "19f025b63662c5fba1976e858b680202",
+    "url": "/pokedex/static/media/special-defense.19f025b6.svg"
+  },
+  {
+    "revision": "cc3bcc6b66cab5c7831c0465269df5da",
+    "url": "/pokedex/static/media/speed.cc3bcc6b.svg"
+  },
+  {
+    "revision": "c369557f25c1b063f1bfe48299e603f1",
+    "url": "/pokedex/static/media/steel.c369557f.svg"
+  },
+  {
+    "revision": "5de56f822618a2004deadc74e98a0d1e",
+    "url": "/pokedex/static/media/water.5de56f82.svg"
+  }
+]);
